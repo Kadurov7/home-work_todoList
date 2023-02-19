67cbfa7f625261d1782088b7883b08245906b0b6
Kadurov7 <kadyrovbekzhan003@gmail.com>
2023-02-19 14:53:02 +0600

    home

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { store } from './store';
+import {  HashRouter } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+
+  <HashRouter>
+   <App />
+  </HashRouter>
+  
   
 );
 
